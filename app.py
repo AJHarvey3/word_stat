a4f07584bfdf5d28429a6438bbb69f860f9c68d4
Alex Harvey <aharvey1@iwu.edu>
2024-02-09 09:50:05 -0600

    fixed some things

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/length.html")
+@app.route("/length")
 def length():
     words = request.args.get("word")
     word_count = len(words.split())
     character = len(words)
     avg_length = character/word_count
-    return render_template("length.html", character=character, word_count=word_count, avg_length=avg_length)git
+    return render_template("length.html", character=character, word_count=word_count, avg_length=avg_length)
 
 
 if __name__ == '__main__':
