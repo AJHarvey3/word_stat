61c7a74e144a8015e9121f573a9a2fe430ec30f9
c-carey <ccarey@iwu.edu>
2024-02-09 09:48:04 -0600

    Updated app.py, index.html, and length.html for routes to work better

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/length.html")
+
+@app.route("/length")
 def length():
     words = request.args.get("word")
     word_count = len(words.split())
     character = len(words)
     avg_length = character/word_count
-    return render_template("length.html", character=character, word_count=word_count, avg_length=avg_length)git
+    return render_template("length.html", character=character, word_count=word_count, avg_length=avg_length)
 
 
 if __name__ == '__main__':
